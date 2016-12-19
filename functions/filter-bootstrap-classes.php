36e9c112d44f9da86a88ac8d621c436013e74e3c
lauren saliba <lauren.saliba@ucf.edu>
2016-12-19 12:41:32 -0500

    CSS Tweaks
    Added new image classes and other minor fixes

--- functions/filter-bootstrap-classes.php
+++ functions/filter-bootstrap-classes.php
@@ -11,4 +11,11 @@ function add_menuclassB($ulclass) {
 }
 add_filter('wp_nav_menu','add_menuclassB');
 
+//Adds responsive class to images
+function image_tag_class($class) {
+    $class .= ' img-fluid';
+    return $class;
+}
+add_filter('get_image_tag_class', 'image_tag_class' );
+
 ?>
