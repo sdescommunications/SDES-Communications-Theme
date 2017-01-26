c8df1329c51116ddd445da37a246364f1104dd03
acardona-ucf <adelino.cardona@ucf.edu>
2017-01-26 14:48:45 -0500

    Notice fix
    More Notice: Undefined offset fixes

--- functions/template-service.php
+++ functions/template-service.php
@@ -25,7 +25,7 @@
 					<strong>Content</strong>
 				</p>
 				<p>	          	
-					<?= (!empty($content)) ? wp_editor( $content[($c-1)][0], 'service_wysiwyg_'. ($c) ) : wp_editor( '', 'service_wysiwyg_'. ($c) ) ?> 
+					<?= (!empty($content)) ? wp_editor( $content[($c-1)][0], 'service_wysiwyg_'. ($c) ) : wp_editor( 'test', 'service_wysiwyg_'. ($c) ) ?> 
 				</p>	          
 			</div>
 
@@ -34,7 +34,7 @@
 					<strong>Image</strong>
 				</p>
 				<p>
-					<input name="service-meta-box-image[]" type="text" value="<?= $url[0][($c-1)] ?>">
+					<input name="service-meta-box-image[]" type="text" value="<?= (!empty($url)) ? $url[0][($c-1)] : false ?>">
 				</p>	          
 			</div>
 			<div class="inside">
@@ -42,7 +42,7 @@
 					<strong>Header</strong>
 				</p>
 				<p>
-					<input name="service-meta-box-header[]" type="text" value="<?= $header[0][($c-1)] ?>">
+					<input name="service-meta-box-header[]" type="text" value="<?= (!empty($header)) ? $header[0][($c-1)] : false ?>">
 				</p>	          
 			</div>
 
@@ -51,7 +51,7 @@
 					<strong>Footer</strong>
 				</p>
 				<p>
-					<input name="service-meta-box-footer[]" type="text" value="<?= $footer[0][($c-1)] ?>">
+					<input name="service-meta-box-footer[]" type="text" value="<?= (!empty($footer)) ? $footer[0][($c-1)] : false ?>">
 				</p>	          
 			</div>
 			<br>
