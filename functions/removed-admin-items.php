7ab80382a5e033ccd4e3cae89c4f75d1ac8965bb
acardona-ucf <adelino.cardona@ucf.edu>
2017-03-27 10:18:49 -0400

    Menus added
    Added Menus for editor uses

--- functions/removed-admin-items.php
+++ functions/removed-admin-items.php
@@ -6,15 +6,24 @@ function remove_menus(){
 	$user = wp_get_current_user();
 	if ( in_array( 'author', (array) $user->roles ) || in_array( 'editor', (array) $user->roles ) ) {
 		    remove_menu_page( 'edit-comments.php' );          //Comments
-		    remove_menu_page( 'themes.php' );                 //Appearance
 		    remove_menu_page( 'edit.php' );                   //Posts
 		    remove_menu_page( 'plugins.php' );                //Plugins
 	 		remove_menu_page( 'upload.php' );                 //Media
 	 		remove_menu_page( 'tools.php' );                  //Tools
 	  		remove_menu_page( 'options-general.php' );        //Settings
+
+	  		// Hide theme selection page
+		    remove_submenu_page( 'themes.php', 'themes.php' );
+		 
+		    // Hide widgets page
+		    remove_submenu_page( 'themes.php', 'widgets.php' );
+		 
+		    // Hide customize page
+		    global $submenu;
+		    unset($submenu['themes.php'][6]);
 	  	}
 	  }
 
-	  add_action( 'admin_menu', 'remove_menus' );
+add_action( 'admin_menu', 'remove_menus' );
 
-	  ?>
+?>
