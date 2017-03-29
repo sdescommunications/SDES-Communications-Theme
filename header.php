e6198bd9df936e65d4b1637e9eaec18024de6a02
acardona-ucf <adelino.cardona@ucf.edu>
2017-03-29 16:18:58 -0400

    Google Analytics
    Google Analytics

--- header.php
+++ header.php
@@ -43,6 +43,24 @@ use SDES\SDES_Static as SDES_Static;
 		var bodyEl = document.getElementsByTagName('body');
 		bodyEl[0].className = bodyEl[0].className.split('nojs').join(' jsEnabled ');
 	</script>
+
+	<?php
+		if (!empty(get_option('google_id'))){
+	?>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', '<?= get_option("google_id") ?>', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+	<?php
+		}
+	?>
+
 	<!-- header -->
 	<div class="container header">
 		<div class="site-title">
