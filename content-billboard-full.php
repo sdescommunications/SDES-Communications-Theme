a29aad90e6a46cf3f9638a0ec8a8ce328f829d23
acardona-ucf <adelino.cardona@ucf.edu>
2018-06-20 14:54:48 -0400

    test

--- content-billboard-full.php
+++ content-billboard-full.php
@@ -12,7 +12,7 @@ get_header();
 
 <!-- content area -->
 <div class="container site-content" id="content">
-	<?= (is_string(get_the_title())) ? '<h1>' . get_the_title() . '</h1><hr>' : null ?>
+	<?= (is_string(get_the_title($post->ID))) ? '<h1>' . get_the_title($post->ID) . '</h1><hr>' : null ?>
 
 	<div class="row">
 		<br>
